571484a45a4cdb5187c53f5c8cce54bc570ddec0
Junior Mayta <junior@Juniors-MBP-2.fritz.box>
2022-08-24 16:17:32 +0200

    Separating into small components main Html render from Applications.

--- src/components/ApplicationView/displayApplication.js
+++ src/components/ApplicationView/displayApplication.js
@@ -47,74 +47,115 @@ class DisplayApplication extends Component {
   
   render() {
     const { classes } = this.props
-    const { applications, currentIndex, currentApplication} = this.state;
-      
-    return (
-      <div className={classes.form}>
-        <Grid container>
-          <Grid item md={4}>
-            <h2>Applications</h2>
-            <div className="list-group">
-              {applications &&
-                applications.map((application, index) => (
-                  <ListItem
-                    selected={index === currentIndex}
-                    onClick={() => this.setActiveApplication(application, index)}
-                    divider
-                    button	
-                    key={index}>
-                    {application.name} - {application.department}
-                  </ListItem>
-                ))}
-            </div>
-          </Grid>
+    const { applications, currentIndex, currentApplication, serviceError} = this.state;
 
-          <Grid item md={6}>
-            {currentApplication ? (
-              <div className={classes.tutorial}>
-                <h4>Detail</h4>
-                <div className={classes.detail}>
-                  <label>
-                    <strong>Application Name:</strong>
-                  </label>{" "}
-                  {currentApplication.name}
-                </div>
-                <div className={classes.detail}>
-                  <label>
-                    <strong>Department:</strong>
-                  </label>{" "}
-                  {currentApplication.department}
-                </div>
-                <div className={classes.detail}>
-                  <label>
-                    <strong>Description:</strong>
-                  </label>{" "}
-                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                </div>
-                <div className={classes.detail}>
-                  <label>
-                    <strong>Status:</strong>
-                  </label>{" "}
-                  Active
-                </div>
 
-                <Link to={"/applications/" + currentApplication.id + "/configurations/add"} className={classes.create}>
-                  Add Configuration
-                </Link>
+    const renderAuthButton = () => {
+      return (
+        <div className={classes.form}>
+          <h2>Service Not available</h2>
+        </div>
+      )
+    }
 
-                <Link to={"/applications/" + currentApplication.id + "/configurations"} className={classes.edit}>
-                  Configurations
-                </Link>
-              </div>
-            ) : (
-                <div>
-                  <br />
-                  <p className={classes.tutorial}>Please Select an Application...</p>
-                </div>
-              )}
-          </Grid>
+    const renderApplications = () => {
+      return (
+        <Grid item md={4}>
+          <h2>Applications</h2>
+          <div className="list-group">
+            {applications &&
+              applications.map((application, index) => (
+                <ListItem
+                  selected={index === currentIndex}
+                  onClick={() => this.setActiveApplication(application, index)}
+                  divider
+                  button	
+                  key={index}>
+                  {application.name} - {application.department}
+                </ListItem>
+              ))}
+          </div>
         </Grid>
-      </div>);
+      )
+    }
+
+    const renderButtons = () => {
+      return (
+        <>
+          <Link 
+            to={"/applications/" + currentApplication.id + "/configurations/add"} 
+            className={classes.create}>
+            Add Configuration
+          </Link>
+          <Link 
+            to={"/applications/" + currentApplication.id + "/configurations"} 
+            className={classes.edit}>
+            Configurations
+          </Link>
+        </>
+      )
+    }
+
+    const displayItem = () => {
+      return (
+          <div className={classes.tutorial}>
+          <h4>Detail</h4>
+          <div className={classes.detail}>
+            <label>
+              <strong>Application Name:</strong>
+            </label>{" "}
+            {currentApplication.name}
+          </div>
+          <div className={classes.detail}>
+            <label>
+              <strong>Department:</strong>
+            </label>{" "}
+            {currentApplication.department}
+          </div>
+          <div className={classes.detail}>
+            <label>
+              <strong>Description:</strong>
+            </label>{" "}
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          </div>
+          <div className={classes.detail}>
+            <label>
+              <strong>Status:</strong>
+            </label>{" "}
+            Active
+          </div>
+
+          {renderButtons()}
+        </div>
+      )
+    }
+
+    const selectItem = () => {
+      return (
+        <div>
+          <br />
+          <p className={classes.tutorial}>Please Select an Application...</p>
+        </div>
+      )
+    }
+
+    if (serviceError) {
+      return renderAuthButton()
+    } else {
+      return (
+        <div className={classes.form}>
+          <Grid container>
+            {renderApplications()}
+            <Grid item md={6}>
+              {currentApplication ? (
+                displayItem()
+              ) : (selectItem())}
+            </Grid>
+          </Grid>
+        </div>
+      )
+    }
+
     }
   }
   
